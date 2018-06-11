9755657d7bdb1cf47b9e01ebf11773120b4a8266
meltem kilic <turquoisemelon@users.noreply.github.com>
2018-06-10 23:36:55 -0400

    Add styled-components

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import styled from "styled-components";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const StyledView = styled.View`
+  flex: 1;
+  background-color: #fff;
+  alignItems: center;
+  justifyContent: center;
+`;
+
+const StyledText = styled.Text`
+  color: #2d174d;
+  fontSize: 18px;
+  margin-bottom: 16px;
+`;
 
 class App extends Component {
   onPressStart() {
@@ -17,15 +22,15 @@ class App extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Find your hair salon by location</Text>
+      <StyledView>
+        <StyledText>Find your hair salon by location</StyledText>
         <Button
           onPress={this.onPressStart}
           title="Let's start"
           color="#90D0F0"
           accessibilityLabel="Find your hair salon by location blue button"
         />
-      </View>
+      </StyledView>
     );
   }
 }
